9b57dee87535ac5b6c0958ad54303c4c21f79df8
YanzWebsiteOn <yanzwebsite@gmail.com>
2024-11-10 21:52:55 +0700

    Update tiktok.js

--- downloader/tiktok.js
+++ downloader/tiktok.js
@@ -47,7 +47,7 @@ module.exports = function(app) {
       const response = await tiktok2(url);
       res.status(200).json({
         status: 200,
-        creator: "Zhizi",
+        creator: "Yanz",
         data: response
       });
     } catch (error) {
