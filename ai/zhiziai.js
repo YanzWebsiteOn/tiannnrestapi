7a93b2b0a1a796335dab2fea0f31419f7b94d669
YanzWebsiteOn <yanzwebsite@gmail.com>
2024-11-16 14:38:01 +0700

    Update zhiziai.js

--- ai/zhiziai.js
+++ ai/zhiziai.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 // Scraper function
 async function getDenisaResponse(text, promptText) {
   try {
-    const url = `https://widipe.com/prompt/gpt?prompt=${encodeURIComponent(promptText)}&text=${encodeURIComponent(text)}`;
+    const url = https://aemt.uk.to/prompt/gpt?prompt=${encodeURIComponent(promptText)}&text=${encodeURIComponent(text)};
     const response = await axios.get(url);
 
     if (response.data.status) {
@@ -19,7 +19,7 @@ async function getDenisaResponse(text, promptText) {
   }
 }
 
-// Endpoint '/lenwyai'
+// Endpoint '/zhiziai'
 app.get('/zhiziai', async (req, res) => {
   try {
     const { text, prompt } = req.query;
