7dcd0142f5312ff3ea4e86c0b8672d620c44c532
YanzWebsiteOn <yanzwebsite@gmail.com>
2025-02-02 00:52:52 +0700

    Update tebaklirik.js

--- games/tebaklirik.js
+++ games/tebaklirik.js
@@ -1,33 +1,31 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 module.exports = function (app) {
 
-  // Full Kode Di Github Saya : https://github.com/Lenwyy/
-
-  async function fetchtebakLirik() {
+  async function fetchTebakLiriks() {
     try {
       const response = await axios.get('https://api.siputzx.my.id/api/games/tebaklirik');
       return response.data;
     } catch (error) {
-      console.error('Error Scraping Games:', error);
+      console.error('Error Scraping tebak lirik', error);
       return null;
     }
   }
 
-  // Endpoint untuk Games Tebak Lirik
+  // Endpoint untuk Games tebak lirik dari API asli
   app.get('/api/games/tebaklirik', async (req, res) => {
     try {
-      const data = await fetchtebakLirik();
-      if (!data) {
-        return res.status(500).json({ error: 'Tidak dapat mengambil data games.' });
+      const data = await fetchTebakLiriks();
+      if (!data || !data.data) {
+        return res.status(500).json({ status: false, error: 'Tidak dapat mengambil data Tebak Lirik.' });
       }
 
       res.status(200).json({
-          data
+        status: true,
+        data: data.data
       });
     } catch (error) {
-      res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data.' });
+      res.status(500).json({ status: false, error: 'Terjadi kesalahan saat mengambil data.' });
     }
   });
 };
