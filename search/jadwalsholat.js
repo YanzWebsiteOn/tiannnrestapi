8cc7328ffbdf9ee2f9bed5d5562b037b1c1eb267
YanzWebsiteOn <yanzwebsite@gmail.com>
2025-02-11 00:52:24 +0700

    Update jadwalsholat.js

--- search/jadwalsholat.js
+++ search/jadwalsholat.js
@@ -63,7 +63,7 @@ module.exports = function(app) {
       const response = await getJadwalSholat(search);
       res.status(200).json({
         status: 200,
-        creator: "Yanz",
+        creator: "Arix",
         data: response
       });
     } catch (error) {
