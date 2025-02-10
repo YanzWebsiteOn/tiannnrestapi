6ec3f6ad891c75364f7c5a5c5cdab5704037a4fa
YanzWebsiteOn <yanzwebsite@gmail.com>
2025-02-10 23:53:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   API_KEYS: {
-    freekey: { key: "freekey", limit: 100, unlimited: false }, // 100 request per hari
+    freekey: { key: "freekey", limit: 200, unlimited: false }, // 100 request per hari
     arixs: { key: "arixs", limit: Infinity, unlimited: true }, // Unlimited request
   },
   RESET_TIME: "00:00", // Reset limit setiap hari pukul 00:00 WIB
