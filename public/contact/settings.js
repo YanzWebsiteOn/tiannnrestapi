d526cf684fbc352e9b2f4b190ecd1d042d134f18
YanzWebsiteOn <yanzwebsite@gmail.com>
2025-02-17 01:52:27 +0700

    Create settings.js

---
+++ public/contact/settings.js
@@ -0,0 +1,5 @@
+// settings.js
+const BOT_TOKEN = '7569646765:AAGtGGiEYMxF7YE-LLsLEO5dJV_fPTyMf3M'; // Ganti dengan token bot Anda
+const CHAT_ID = '7472554514'; // Ganti dengan chat ID Anda
+
+export { BOT_TOKEN, CHAT_ID };
